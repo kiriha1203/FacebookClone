3566777d3eb3341b4aff946eda02e06da3ff649f
takefumi mizumoto <mizumoto.takefumi@gmail.com>
2020-03-23 11:47:54 +0900

    connect user and task

--- Gemfile
+++ Gemfile
@@ -61,3 +61,7 @@ group :development do
 end
 
 gem 'bcrypt'
+
+group :development do
+  gem 'dotenv-rails'
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
 
   root to: 'tasks#index'
-  resources :tasks
+  resources :tasks do
+    collection do
+      post :confirm
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
